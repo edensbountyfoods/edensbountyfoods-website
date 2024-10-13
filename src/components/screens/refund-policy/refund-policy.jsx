594bb77bb72ffa54b94cd87cd8933e6610661dcf
Raja <raja.rathinam@pcisage.com>
2024-10-13 12:17:08 +0530

    new desigh repo migration

--- src/components/screens/refund-policy/refund-policy.jsx
+++ src/components/screens/refund-policy/refund-policy.jsx
@@ -7,10 +7,9 @@ const RefundPolicyScreen = () => {
     <div className={styles.TermScreen}>
       <CustomContainer>
         <div className={styles.wrap}>
-          <h1>Privacy Policy</h1>
+          <h1>Refund Policy</h1>
           <br />
           <br />
-
           <div className={styles.cont}>
             <div class="entry-content single-content">
               <p>
@@ -183,6 +182,88 @@ const RefundPolicyScreen = () => {
                 </strong>
               </p>
             </div>
+          </div>
+        </div>
+        <div className={styles.wrap}>
+          <h1>Shipping Policy</h1>
+          <br />
+          <br />
+          <div className={styles.cont}>
+            Last updated October 11, 2024
+            <p>
+              This Shipping & Delivery Policy is part of our Terms and
+              Conditions and should be read alongside our main Terms: Legal
+              terms page url.
+            </p>
+            <p>
+              Please carefully review our Shipping & Delivery Policy when
+              purchasing our products. This policy will apply to any order you
+              place with us.
+            </p>
+            <h3 class="wp-block-heading">
+              <strong>WHAT ARE MY SHIPPING & DELIVERY OPTIONS?</strong>
+            </h3>
+            <p>
+              We offer various shipping options. In some cases, a third-party
+              supplier may be managing our inventory and will be responsible for
+              shipping your products.
+            </p>
+            <h3>
+              <strong>Shipping Fees</strong>
+            </h3>
+            We offer shipping at the following rates:
+            <ul>
+              <li>Standard: 3-4 business days</li>
+            </ul>
+            ________________
+            <p>
+              All times and dates given for delivery of the products are given
+              in good faith but are estimates only.
+            </p>
+            <h3>
+              <strong>HOW IS MY SUBSCRIPTION FULFILLED?</strong>
+            </h3>
+            <p>
+              If you are buying a subscription, then we will deliver on: Weekly
+              subscription available.
+            </p>
+            <h3>
+              <strong>DO YOU DELIVER INTERNATIONALLY?</strong>
+            </h3>
+            <p>We do not offer international shipping.</p>
+            <h3>
+              <strong>WHAT HAPPENS IF MY ORDER IS DELAYED?</strong>
+            </h3>
+            <p>
+              If delivery is delayed for any reason, we will let you know as
+              soon as possible and will advise you of a revised estimated date
+              for delivery.
+            </p>
+            <p>QUESTIONS ABOUT RETURNS?</p>
+            <p>
+              If you have questions about returns, please review our Return
+              Policy
+            </p>
+            <h3>
+              <strong>HOW CAN YOU CONTACT US ABOUT THIS POLICY?</strong>
+            </h3>
+            <p>
+              If you have any further questions or comments, you may contact us
+              by:
+            </p>
+            <p>
+              <strong>Email</strong>:{" "}
+              <strong>
+                <a href="mailto:info@edensbountyfoods.com">
+                  info@edensbountyfoods.com
+                </a>
+              </strong>
+              <br />
+              <strong>Phone</strong>:{" "}
+              <strong>
+                <a href="tel:+91 9940928610">+91 9940928610</a>
+              </strong>
+            </p>
           </div>
         </div>
       </CustomContainer>
