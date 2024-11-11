0c72c6ba20e01c9df746fc488b10f39146c9a3f1
Raja <raja.rathinam@pcisage.com>
2024-11-11 21:50:22 +0530

    fix

--- src/pages/checkout/[merchantTransactionId]/index.jsx
+++
@@ -1,8 +0,0 @@
-import PaymentStatusScreen from "@/components/screens/checkout/status/status";
-import React from "react";
-
-const PaymentStatusPage = () => {
-  return <PaymentStatusScreen />;
-};
-
-export default PaymentStatusPage;
